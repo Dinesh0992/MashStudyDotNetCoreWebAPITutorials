c5e81cea90f9bf27f8562a917f9c5444ba0f59da
Dinesh.V <dineshviswes@gmail.com>
2021-05-21 11:53:56 +0530

    Implement add and delete operation in WebAPI

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MashStudyDotNetCoreWebAPITutorials.Data;
+using Microsoft.EntityFrameworkCore;
+using MashStudyDotNetCoreWebAPITutorials.Models;
 
 namespace MashStudyDotNetCoreWebAPITutorials.Controllers
 {
@@ -20,10 +22,39 @@ namespace MashStudyDotNetCoreWebAPITutorials.Controllers
 
 
         [HttpGet("")]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            var cities = dc.Cities.ToList();
+            var cities = await dc.Cities.ToListAsync();
             return Ok(cities);
+        }
+
+        [HttpPost("addcity/{cityname}")]
+        public async Task<IActionResult> AddCity(string CityName)
+        {
+            City newcity = new City();
+            newcity.Name = CityName;
+            await dc.Cities.AddAsync(newcity);
+            await dc.SaveChangesAsync();
+            return Ok(newcity);
+        }
+
+        [HttpPost("post/addcity")]
+        public async Task<IActionResult> AddCity([FromBody] City newcity)
+        {
+            // City newcity = new City();
+            // newcity.Name = CityName;
+            await dc.Cities.AddAsync(newcity);
+            await dc.SaveChangesAsync();
+            return Ok(newcity);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await dc.Cities.FindAsync(id);
+            dc.Cities.Remove(city);
+            await dc.SaveChangesAsync();
+            return Ok(id);
         }
 
         [HttpGet("{id}")]
