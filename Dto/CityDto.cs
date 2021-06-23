a6b4837f1ffd41ea9982ff75282e31a9dc705eb8
Dinesh.V <dineshviswes@gmail.com>
2021-06-23 14:45:01 +0530

    Input validation in ASP.NET Web API

--- Dto/CityDto.cs
+++ Dto/CityDto.cs
@@ -1,11 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MashStudyDotNetCoreWebAPITutorials.Dto
 {
     public class CityDto
     {
-         public int Id { get; set; }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name field is Mandatory")]
+        [StringLength(10, MinimumLength = 2)]
+        [RegularExpression(".*[a-zA-Z]+.*",ErrorMessage ="Only numerics are not  allowed")]
         public string Name { get; set; }
+
+        [Required]
         public string Country { get; set; }
     }
 }
